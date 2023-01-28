006be94f37540b5fdd2e9130891a0ec84fdcd70a
Oswaldo Leal <ojlr.1998@gmail.com>
2023-01-28 14:07:29 -0400

    Added comment to userConfigView

--- api/views/user_config.py
+++ api/views/user_config.py
@@ -14,3 +14,20 @@ class UserConfigView(generics.CreateAPIView):
             'JWK URL': 'http://127.0.0.1:9001/jwks/',
         }
         return Response(response)
+        """
+        This should return
+        config_values = {
+            'redirect_uri': 'https://backend.com/canvas/launch/',
+            'title': LTI_TOOL_NAME,
+            'description': LTI_TOOL_DESCRIPTION,
+            'target_uri': 'https://backend.com/canvas/launch/',
+            'openid_url': 'https://backend.com/canvas/login/',
+            'jwk_method': {
+                'public_jwk': CURRENT_DEPLOYMENT_JWK,
+                'public_jwk_url': CURRENT_DEPLOYMENT_JWK_URL
+            },
+            'lti_advantage_services': [
+                LIST_OF_OPTIONS_TO_BE_TURNED_ON
+            ]
+        }
+        """
