c476084cb0b26f3c30e77619761f9686ad95e93d
Oswaldo Leal <38363177+oswaldoleal@users.noreply.github.com>
2023-01-28 12:30:01 -0400

    LTI Redirect (#1)
    * Added login and launch request handlers
    
    * Added cors and dummy lti config endpoint
    
    * Added cors and dummy lti config endpoint
    
    * Update settings.py
    
    ---------
    
    Co-authored-by: julianuri <julian.uribe.contact@gmail.com>

--- canvas/views/launch_view.py
+++ canvas/views/launch_view.py
@@ -1,9 +1,31 @@
-from rest_framework import generics
-from rest_framework.response import Response
+from django.shortcuts import redirect
+from pylti1p3.contrib.django import DjangoDbToolConf
+from pylti1p3.contrib.django import (
+    DjangoMessageLaunch,
+    DjangoCacheDataStorage,
+)
+from pylti1p3.contrib.django import DjangoDbToolConf
+
+from django.shortcuts import redirect
+from rest_framework.views import APIView
 
 
-class LaunchView(generics.GenericAPIView):
+class LaunchView(APIView):
+
+    def get_launch_url(self, request):
+        target_link_uri = request.POST.get(
+            "target_link_uri", request.GET.get("target_link_uri")
+        )
+        if not target_link_uri:
+            raise Exception('Missing "target_link_uri" param')
+        return target_link_uri
 
     def post(self, request, *args, **kwargs):
-        msg = '<h1>Test message<h1/>'
-        return Response(msg)
+        tool_conf = DjangoDbToolConf()
+        launch_data_storage = DjangoCacheDataStorage()
+        message_launch = DjangoMessageLaunch(
+            request, tool_conf, launch_data_storage=launch_data_storage
+        )
+
+        message_launch_data = message_launch.get_launch_data()
+        return redirect("https://localhost:3000/lti-config")
--- canvas/views/login_view.py
+++ canvas/views/login_view.py
@@ -1,9 +1,25 @@
-from rest_framework import generics
-from rest_framework.response import Response
+from pylti1p3.contrib.django import (
+    DjangoOIDCLogin,
+    DjangoCacheDataStorage,
+)
+from pylti1p3.contrib.django import DjangoDbToolConf
+from rest_framework.views import APIView
 
 
-class LoginView(generics.GenericAPIView):
+class LoginView(APIView):
+    def get_launch_url(self, request):
+        target_link_uri = request.POST.get(
+            'target_link_uri', request.GET.get('target_link_uri')
+        )
+        if not target_link_uri:
+            raise Exception('Missing "target_link_uri" param')
+        return target_link_uri
 
-    def get(self, request, *args, **kwargs):
-        msg = '<h1>Test message<h1/>'
-        return Response(msg)
+    def post(self, request, *args, **kwargs):
+        tool_conf = DjangoDbToolConf()
+        launch_data_storage = DjangoCacheDataStorage()
+        oidc_login = DjangoOIDCLogin(
+            request, tool_conf, launch_data_storage=launch_data_storage
+        )
+        target_link_uri = self.get_launch_url(request)
+        return oidc_login.redirect(target_link_uri)
